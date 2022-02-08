8dbb39d635efaf6b5739cfef0fcde97f4cb139c9
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-02-09 03:42:25 +0500

    Season 1| Episode 3

---
+++ Flutter Bootcamp/Season 1/Episode 3/episode3.dart
@@ -0,0 +1,16 @@
+// Printing Statements in Dart
+main(){
+  // Variable Initialization
+  String instructor = 'Usama Sarwar';
+  String course = 'Flutter Bootcamp';
+  String organization = '<\\> DevNation';
+  int seasonNo = 1;
+  int episodeNo = 3;
+  
+  // Printing
+  print('Season $seasonNo | Episode $episodeNo\n$course \nInstructed by $instructor \nPowered by $organization');
+  
+  print('DevNation\'s Instructor'); // I'm an Instructor
+  print("DevNation's Instructor"); // I'm an Instructor
+  print("The name of Instructor is \"Usama Sarwar\""); // I'm an Instructor
+}
--- Flutter/structure/android/app/src/main/kotlin/com/example/structure/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.structure
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
